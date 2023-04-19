ecdedfe158998f110bf7955813affbd534ac3034
McKenna <mckenna219@outlook.com>
2023-04-19 18:34:41 -0500

    Prep 2 complete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Request grade percentage from user
+        Console.Write("What is your grade percentage? ");
+        string userAnswer = Console.ReadLine();
+        int userPercentage = int.Parse(userAnswer);
+        string letterGrade = "";
+
+        //Convert user input (percentage) to letter grade
+        if (userPercentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (userPercentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (userPercentage >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (userPercentage >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+        Console.WriteLine($"Your letter grade is: {letterGrade}.");
+        
+        if (userPercentage > 70)
+        {
+            Console.WriteLine("Congrats! You passed the class.");
+        }        
     }
-}
+
+
+        
+    }
