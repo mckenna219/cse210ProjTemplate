fe379231a6521d3496df63921da552eefabcead7
McKenna <mckenna219@outlook.com>
2023-04-19 16:26:40 -0500

    Prep #1 Complete

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Ask user for first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Ask user for last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Insert blank line
+        Console.WriteLine();
+
+        //Display name of user in Bond, James Bond version
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
