da9cc0e78b13464db519273438f12959e3f9e740
McKenna <mckenna219@outlook.com>
2023-04-19 23:51:34 -0500

    Core Req #1 Complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Ask user for the magic number.
+        Console.Write("What is the magic number? ");
+        string userMagicNum = Console.ReadLine();
+        int magicNumber = int.Parse(userMagicNum);
+
+        // Ask the user for a guess
+        Console.Write("What is your guess? ");
+        string userGuessNum = Console.ReadLine();
+        int guessNumber = int.Parse(userGuessNum);
+        string displayPrompt = "";
+
+        // Determine if user needs to guess higher or lower 
+        if (guessNumber > magicNumber)
+        {
+            displayPrompt = "Lower";
+        }
+        else if (guessNumber < magicNumber)
+        {
+            displayPrompt = "Higher";
+        }
+        else
+        {
+            displayPrompt = "You guessed it!";
+        }
+        Console.WriteLine($"{displayPrompt}");
+
+
+
     }
 }
