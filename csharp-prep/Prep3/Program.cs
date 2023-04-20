d1d397ed0d0d1c87fdfb3ef156b454e30b9ed203
McKenna <mckenna219@outlook.com>
2023-04-20 00:17:34 -0500

    Core Req #2 Complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -8,29 +8,31 @@ class Program
         Console.Write("What is the magic number? ");
         string userMagicNum = Console.ReadLine();
         int magicNumber = int.Parse(userMagicNum);
+        int guessNumber = 0;
 
-        // Ask the user for a guess
-        Console.Write("What is your guess? ");
-        string userGuessNum = Console.ReadLine();
-        int guessNumber = int.Parse(userGuessNum);
-        string displayPrompt = "";
-
-        // Determine if user needs to guess higher or lower 
-        if (guessNumber > magicNumber)
+        // Loop as long as guess does not match magic number
+        while (guessNumber != magicNumber)
         {
-            displayPrompt = "Lower";
-        }
-        else if (guessNumber < magicNumber)
-        {
-            displayPrompt = "Higher";
-        }
-        else
-        {
-            displayPrompt = "You guessed it!";
-        }
-        Console.WriteLine($"{displayPrompt}");
+            // Ask the user for a guess
+            Console.Write("What is your guess? ");
+            string userGuessNum = Console.ReadLine();
+            guessNumber = int.Parse(userGuessNum);
+            string displayPrompt = "";
 
-
-
+            // Determine if user needs to guess higher or lower 
+            if (guessNumber > magicNumber)
+            {
+                displayPrompt = "Lower";
+            }
+            else if (guessNumber < magicNumber)
+            {
+                displayPrompt = "Higher";
+            }
+            else
+            {
+                displayPrompt = "You guessed it!";
+            }
+            Console.WriteLine($"{displayPrompt}");
+        }
     }
 }
