2d00df80b6ea750e3b5d66f4b22ca9e2391c03d0
McKenna <mckenna219@outlook.com>
2023-04-21 13:58:30 -0500

    Prep #5 Complete

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+        string userName = GetUserName();
+        int faveNum = GetFavoriteNumber();
+        int squaredNum = GetSquaredNumber(faveNum);
+        DisplayResult(userName, squaredNum);
+    }
+
+    // Display welcome message
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+
+    // Prompt user for name
+    static string GetUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+
+        return userName;
+    }
+
+
+    // Prompt user for number
+    static int GetFavoriteNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string favoriteNumber = Console.ReadLine();
+        int faveNum = int.Parse(favoriteNumber);
+
+        return faveNum;
+    }
+
+
+    // Square users favorite number
+    static int GetSquaredNumber(int faveNum)
+    {
+        int squaredNum = faveNum * faveNum;
+
+        return squaredNum;
+    }
+
+
+    // Display result
+    static void DisplayResult(string userName, int squaredNum)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squaredNum}.");
     }
 }
